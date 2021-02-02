3b63a98059a64ec0429cde7ce7a7bfa4451cdcb5
samedson <samedson@google.com>
2021-02-02 16:47:47 -0500

    Issue reproduction for https://github.com/firebase/firebase-ios-sdk/issues/7400

---
+++ crashlytics/CrashlyticsExample/ViewController.m
@@ -0,0 +1,60 @@
+//
+//  Copyright (c) 2018 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "ViewController.h"
+@import FirebaseCore;
+@import FirebaseCrashlytics;
+
+#import <CrashlyticsExample-Swift.h>
+
+@import Vision;
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+- (IBAction)initiateCrash:(id)sender {
+
+  [[FIRCrashlytics crashlytics] log:@"Cause Crash button clicked"];
+
+    NSLog(@"Calling performRequests SWIFT");
+    [[[VisionTest alloc] init] doStuff];
+    
+  if (@available(iOS 13.0, *)) {
+      NSLog(@"Calling performRequests OBJC");
+
+      
+    CGSize size = CGSizeMake(64, 64);
+    UIGraphicsBeginImageContextWithOptions(size, YES, 0);
+    [[UIColor whiteColor] setFill];
+    UIRectFill(CGRectMake(0, 0, size.width, size.height));
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    VNImageRequestHandler *handler = [[VNImageRequestHandler alloc] initWithCGImage:image.CGImage options:@{}];
+    VNGenerateAttentionBasedSaliencyImageRequest *request = [[VNGenerateAttentionBasedSaliencyImageRequest alloc] init];
+    NSError *error = nil;
+    [handler performRequests:@[request] error:&error];
+
+        
+  } else {
+    NSLog(@"Did not call");
+  }
+}
+
+@end
---
+++ crashlytics/CrashlyticsExample/VisionTest.swift
@@ -0,0 +1,34 @@
+//
+//  VisionTest.swift
+//  CrashlyticsExample
+//
+//  Created by Sam Edson on 1/28/21.
+//  Copyright © 2021 Google Inc. All rights reserved.
+//
+
+import Foundation
+import Vision
+
+class VisionTest: NSObject {
+  @objc public func doStuff() {
+
+
+
+//    VNImageRequestHandler(cgImage: CGImage(, options: [])
+
+    
+//    CGSize size = CGSizeMake(64, 64);
+//    UIGraphicsBeginImageContextWithOptions(size, YES, 0);
+//    [[UIColor whiteColor] setFill];
+//    UIRectFill(CGRectMake(0, 0, size.width, size.height));
+//    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//
+//
+//
+//
+//    VNImageRequestHandler *handler = [[VNImageRequestHandler alloc] initWithCGImage:image.CGImage options:@{}];
+//      VNGenerateAttentionBasedSaliencyImageRequest *request = [[VNGenerateAttentionBasedSaliencyImageRequest alloc] init];
+//    [handler performRequests:@[request] error:nil];
+  }
+}
